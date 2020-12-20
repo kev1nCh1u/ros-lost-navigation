5f13644072b9774bd250fbc911888a7479e4724f
KevinChiu <fsd83811@gmail.com>
2020-12-20 12:03:29 +0800

    lostCalc while

--- lost_navigation/src/lost.cpp
+++ lost_navigation/src/lost.cpp
@@ -4,6 +4,7 @@
 #include "nav_msgs/OccupancyGrid.h"
 #include "sensor_msgs/LaserScan.h"
 #include <sensor_msgs/point_cloud_conversion.h>
+#include <nav_msgs/Odometry.h>
 
 #include <laser_geometry/laser_geometry.h>
 #include <tf/transform_listener.h>
@@ -21,16 +22,18 @@ class Lost
 private:
   // ros
   ros::NodeHandle n;
-  ros::Time now;
+  ros::Timer timer;
   ros::Subscriber subMap;
   ros::Subscriber subScan;
-  ros::Publisher pointPub;
-  ros::Timer timer;
+  ros::Subscriber subOdom;
+  // ros::Publisher pointPub;
 
   // fuc
   void timerCallback(const ros::TimerEvent &);
   void mapCallback(const nav_msgs::OccupancyGrid &msg);
   void scanCallback(const sensor_msgs::LaserScan &msg);
+  void odomCallback(const nav_msgs::Odometry &msg);
+  void lostCalc();
 
   // var
   nav_msgs::OccupancyGrid mapMsg;
@@ -49,88 +52,120 @@ public:
  * ************************************************************************************************************/
 Lost::Lost(/* args */)
 {
-  timer = n.createTimer(ros::Duration(5.0), &Lost::timerCallback, this);
+  // timer = n.createTimer(ros::Duration(1.0), &Lost::timerCallback, this);
   subMap = n.subscribe("map", 1000, &Lost::mapCallback, this);
-  subScan = n.subscribe("scan", 1000, &Lost::scanCallback, this);
-  pointPub = n.advertise<sensor_msgs::PointCloud2>("point", 0, this);
-  now = ros::Time(0);
+  subScan = n.subscribe("scan", 10000, &Lost::scanCallback, this);
+  // subOdom = n.subscribe("odom", 1000, &Lost::odomCallback, this);
+  // pointPub = n.advertise<sensor_msgs::PointCloud>("point", 0, this);
+  
+  Lost::lostCalc();
 }
 
 Lost::~Lost()
 {
 }
 
+void Lost::lostCalc()
+{
+  ros::Rate loop_rate(1);
+  int loopCount = 0;
+  while (ros::ok())
+  {
+    // std::cout << "================================================" << std::endl;
+
+    // map msg
+    // std::cout << "map:" << mapMsg.info << std::endl;
+    // std::cout << "map size:" << mapMsg.data.size() << std::endl;
+    // std::cout << "map pos:";
+    // for(int i = 0; i <  mapMsg.data.size(); i++)
+    // {
+    //   if(mapMsg.data[i] == 100)
+    //   {
+    //     std::cout << i << "/" << i % mapMsg.info.width << "/" << i / mapMsg.info.width << ", " ;
+    //   }
+    // }
+    // std::cout << std::endl;
+
+    // scan msg
+    // std::cout << "scan size:" << scanMsg.ranges.size() << std::endl;
+    sensor_msgs::PointCloud cloud;
+    sensor_msgs::PointCloud2 cloud2;
+    projector_.projectLaser(scanMsg, cloud);
+    projector_.projectLaser(scanMsg, cloud2);
+
+    try
+    {
+      sensor_msgs::PointCloud base_cloud;
+
+      // tf
+      listener.waitForTransform("/map", "/base_link", ros::Time(0), ros::Duration(0.1));
+      listener.lookupTransform("/map", "/base_link", ros::Time(0), transformVec);
+      float orgX = transformVec.getOrigin().x();
+      float orgY = transformVec.getOrigin().y();
+      std::cout << loopCount << "===============================================================" << std::endl;
+      std::cout << "tf_org xy:" << orgX << " " << orgY << std::endl;
+
+      base_cloud = cloud;
+      for (int i = 0; i < base_cloud.points.size(); i++)
+      {
+        base_cloud.points[i].x = cloud.points[i].x + orgX;
+        base_cloud.points[i].y = cloud.points[i].y + orgY;
+      }
+
+      // pcl tf
+      // sensor_msgs::PointCloud2 base_cloud2;
+      // pcl_ros::transformPointCloud("/map", cloud2, base_cloud2, listener);
+      // sensor_msgs::convertPointCloud2ToPointCloud(base_cloud2, base_cloud);
+      // std::cout << "base_link xy:" << base_cloud.points[0].x << " " << base_cloud.points[0].y << std::endl;
+
+      int lostCount = 0;
+      for (int i = 0; i < base_cloud.points.size(); i++)
+      {
+        float width = (base_cloud.points[i].x - mapMsg.info.origin.position.x) / mapMsg.info.resolution;
+        float height = mapMsg.info.height - (base_cloud.points[i].y - mapMsg.info.origin.position.y) / mapMsg.info.resolution;
+        int data_num = width + height * mapMsg.info.width;
+        if (mapMsg.data[data_num] == 100)
+        {
+          lostCount++;
+          // std::cout << "base_cloud pixel wh:" << width << height << std::endl;
+          // std::cout << "base_cloud size:" << base_cloud.points.size() << std::endl;
+        }
+      }
+      float lostRate = float(base_cloud.points.size() - lostCount) / base_cloud.points.size() * 100;
+      std::cout << "lostCount:" << lostCount << " lostRate:" << lostRate << " " << std::endl;
+
+      // pointPub.publish(base_cloud);
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++loopCount;
+  }
+}
+
 /***************************************************************************************************************************************
 * timer
 * ***************************************************************************************************************************************/
-void Lost::timerCallback(const ros::TimerEvent &)
-{
-  now = ros::Time(0);
+// void Lost::timerCallback(const ros::TimerEvent &)
+// {
+// }
 
-  // std::cout << "================================================" << std::endl;
+/***************************************************************************************************************************************
+* odom subscribe
+* ***************************************************************************************************************************************/
 
-  // map msg
-  // std::cout << "map:" << mapMsg.info << std::endl;
-  // std::cout << "map size:" << mapMsg.data.size() << std::endl;
-  // std::cout << "map pos:";
-  // for(int i = 0; i <  mapMsg.data.size(); i++)
-  // {
-  //   if(mapMsg.data[i] == 100)
-  //   {
-  //     std::cout << i << "/" << i % mapMsg.info.width << "/" << i / mapMsg.info.width << ", " ;
-  //   }
-  // }
-  // std::cout << std::endl;
-
-  // scan msg
-  // std::cout << "scan size:" << scanMsg.ranges.size() << std::endl;
-  sensor_msgs::PointCloud cloud;
-  sensor_msgs::PointCloud2 cloud2;
-  projector_.projectLaser(scanMsg, cloud);
-  projector_.projectLaser(scanMsg, cloud2);
-
-  // tf
-  // listener.waitForTransform("/map", "/base_link", now, ros::Duration(1.0));
-  // listener.lookupTransform("/map", "/base_link", ros::Time(0), transformVec);
-  // std::cout << "tf_org xy:" << transformVec.getOrigin().x() << " " << transformVec.getOrigin().y() << std::endl;
-
-  // pcl tf
-  try
-  {
-    sensor_msgs::PointCloud2 base_cloud2;
-    sensor_msgs::PointCloud base_cloud;
-    pcl_ros::transformPointCloud("/map", cloud2, base_cloud2, listener);
-    sensor_msgs::convertPointCloud2ToPointCloud(base_cloud2, base_cloud);
-    std::cout << "base_link xy:" << base_cloud.points[0].x << " " << base_cloud.points[0].y << std::endl;
-    pointPub.publish(base_cloud2);
-
-    std::cout << "base_cloud pixel w:" << (base_cloud.points[0].x - mapMsg.info.origin.position.x) / mapMsg.info.resolution << std::endl;
-    std::cout << "base_cloud pixel h:" << mapMsg.info.height - (base_cloud.points[0].y - mapMsg.info.origin.position.y) / mapMsg.info.resolution << std::endl;
-    std::cout << "base_cloud size:" << base_cloud.points.size() << std::endl;
-    int count = 0;
-    for (int i = 0; i < base_cloud.points.size(); i++)
-    {
-      float width = (base_cloud.points[i].x - mapMsg.info.origin.position.x) / mapMsg.info.resolution;
-      float height = mapMsg.info.height - (base_cloud.points[i].y - mapMsg.info.origin.position.y) / mapMsg.info.resolution;
-      int data_num = width + height * mapMsg.info.width;
-      if (mapMsg.data[data_num] == 100)
-        count++;
-    }
-    float lost = float(base_cloud.points.size() - count) / base_cloud.points.size()  * 100;
-    std::cout << "count:" << count << " lost:" << lost << " " << std::endl;
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << e.what() << '\n';
-  }
-}
+// void Lost::odomCallback(const nav_msgs::Odometry &msg)
+// {
+// }
 
 /**************************************************************************************************************
  * map subscribe
  * ************************************************************************************************************/
 void Lost::mapCallback(const nav_msgs::OccupancyGrid &msg)
 {
-  std::cout << "test" << std::endl;
   mapMsg = msg;
 }
 
@@ -153,6 +188,8 @@ int main(int argc, char **argv)
   Lost lost;
 
   ros::spin();
+  // ros::MultiThreadedSpinner spinner(10);
+  // spinner.spin();
 
   return 0;
 }
